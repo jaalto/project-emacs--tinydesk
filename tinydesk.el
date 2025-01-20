47ca2e9e4713f5fdd9fb46450c8c048f2c4cb652
Jari Aalto <jari.aalto@cante.net>
2025-01-20 13:15:56 +0200

    tinydesk.el: copyright update

--- tinydesk.el
+++ tinydesk.el
@@ -1,5 +1,5 @@
 ;;; tinydesk.el --- Save and restore files between Emacs sessions
-;; Copyright (C) 1995-2013 Jari Aalto
+;; Copyright (C) 1995-2025 Jari Aalto
 
 ;; Author:	Jari Aalto
 ;; Maintainer:	Jari Aalto
